2bcc5700414c824085a6ec930179bd1dc38f79dd
stompesi <stompesi@gmail.com>
2025-05-02 09:04:55 +0000

    Update shell script

--- utils/deploy/02_deploy-burner-router.sh
+++ utils/deploy/02_deploy-burner-router.sh
@@ -2,11 +2,12 @@
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 PARENT_PATH="$(dirname "$(dirname "$SCRIPT_PATH")")"
 
-cd "$SCRIPT_PATH"
+source $SCRIPT_PATH/../env.sh
+source $SCRIPT_PATH/../utils.sh
 
-
-source $PARENT_PATH/utils/env.sh
-source $PARENT_PATH/utils/utils.sh
+if [[ "$IS_LOCAL_BLOCKCHAIN" == "true" ]]; then
+  start_anvil_docker $LATEST_STATE_PATH/$CHAIN_ID $LATEST_STATE_PATH/$CHAIN_ID
+fi
 
 cd $PROJECT_ROOT_PATH
 
